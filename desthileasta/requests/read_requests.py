4220327359f85e6e9f07c5ca1f96af4481628c74
Ronald Lugo <r.lugoq@uniandes.edu.co>
2024-02-22 13:37:56 -0500

    Test wizard

--- desthileasta/requests/read_requests.py
+++ desthileasta/requests/read_requests.py
@@ -30,36 +30,11 @@ while not(is_last_page):
   total_option_list += count_option_list
   total_option_wizard += count_option_wizard
   print("page {}".format(page))
-  print(count_selected)
-  print(count_option_list)
-  print(count_option_wizard)
+  print("selected: {}".format(count_selected))
+  print("list: {}".format(count_option_list))
+  print("wizard: {}".format(count_option_wizard))
 
 print("totales")
-print(total_selected)
-print(total_option_list)
-print(total_option_wizard)
-print(is_last_page)
-
-
-""" url = 'https://webhook.site/token/414559c2-a5ad-47c3-82ac-18155e56f4d9/requests?per_page=50&page=1'
-headers = {'accept': 'application/json', 'api-key': '414559c2-a5ad-47c3-82ac-18155e56f4d9'}
-response  = requests.get(url, headers=headers)
-if response.status_code == 200:
-  json_data = response.json()
-  if(json_data["is_last_page"]):
-    print("is last page")
-  else:
-    print("not is last page")
-  
-  
-  content = [row["content"] for row in json_data["data"]]
-  selected = [json.loads(item)["selected"] for item in content]
-  
-  count_selected = len(selected)
-  count_option_list = selected.count('list')
-  count_option_wizard = selected.count('wizard')
-  
-  print("Respuestas: {}, list: {}, wizard: {}".format(count_selected, count_option_list, count_option_wizard))
-  print(json_data)
-else:
-  print('Error:', response.status_code) """
+print("selected: {}".format(total_selected))
+print("list: {}".format(total_option_list))
+print("wizard: {}".format(total_option_wizard))
